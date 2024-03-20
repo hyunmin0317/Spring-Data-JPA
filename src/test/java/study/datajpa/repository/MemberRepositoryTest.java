2fccf8632d7866dbea343d25173ba2b84e9a6ce7
hyunmin0317 <choihm9903@naver.com>
2024-03-20 21:58:06 +0900

    feat: 파라미터 바인딩 (#3)

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -9,6 +9,7 @@ import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 import study.datajpa.entity.Team;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -107,7 +108,7 @@ class MemberRepositoryTest {
     public void testQuery() throws Exception {
         //given
         Member m1 = new Member("AAA", 10);
-        Member m2 = new Member("AAA", 20);
+        Member m2 = new Member("BBB", 20);
         memberRepository.save(m1);
         memberRepository.save(m2);
 
@@ -123,7 +124,7 @@ class MemberRepositoryTest {
     public void findUsernameList() throws Exception {
         //given
         Member m1 = new Member("AAA", 10);
-        Member m2 = new Member("AAA", 20);
+        Member m2 = new Member("BBB", 20);
         memberRepository.save(m1);
         memberRepository.save(m2);
 
@@ -149,5 +150,20 @@ class MemberRepositoryTest {
 
         //then
         memberDtoList.forEach(System.out::println);
+    }
+
+    @Test
+    public void findByNames() throws Exception {
+        //given
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("BBB", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        //when
+        List<Member> result = memberRepository.findByNames(Arrays.asList("AAA", "BBB"));
+
+        //then
+        result.forEach(System.out::println);
     }
 }
