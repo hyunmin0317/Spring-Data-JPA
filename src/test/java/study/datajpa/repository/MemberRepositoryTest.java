67eb4243e773940657f7360d3aabcc9f7aa79078
hyunmin0317 <choihm9903@naver.com>
2024-03-21 20:10:49 +0900

    test: @EntityGraph (#4)

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -270,4 +270,41 @@ class MemberRepositoryTest {
         //then
         assertThat(resultCount).isEqualTo(3);
     }
+
+    @Test
+    public void findMemberLazy() throws Exception {
+        //given
+        //member1 -> teamA
+        //member2 -> teamB
+
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        teamRepository.save(teamA);
+        teamRepository.save(teamB);
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 10, teamA);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        em.flush();
+        em.clear();
+
+        //FetchType.LAZY N + 1
+        //select Member 1
+        List<Member> members = memberRepository.findAll();
+        members.forEach(member -> {
+            System.out.println("member = " + member.getUsername());
+            System.out.println("member.teamClass = " + member.getTeam().getClass());
+            System.out.println("member.team = " + member.getTeam().getName());
+        });
+
+        //fetch join
+        List<Member> fetchMembers = memberRepository.findMemberFetchJoin();
+        fetchMembers.forEach(member -> {
+            System.out.println("member = " + member.getUsername());
+            System.out.println("member.team = " + member.getTeam().getName());
+        });
+
+        //then
+    }
 }
