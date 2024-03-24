0693d9f99b1e21cf6d9b195a446df88d6ecabf76
hyunmin0317 <choihm9903@naver.com>
2024-03-24 13:11:52 +0900

    feat: Web 확장 - 페이징과 정렬 (#5)

--- src/main/java/study/datajpa/controller/MemberController.java
+++ src/main/java/study/datajpa/controller/MemberController.java
@@ -2,11 +2,17 @@ package study.datajpa.controller;
 
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 import study.datajpa.repository.MemberRepository;
+
+import java.util.stream.IntStream;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,8 +31,16 @@ public class MemberController {
         return member.getUsername();
     }
 
+    @GetMapping("/members")
+    public Page<MemberDto> list(@PageableDefault(size = 5) Pageable pageable) {
+        return memberRepository.findAll(pageable)
+                .map(MemberDto::from);
+    }
+
     @PostConstruct
     public void init() {
-        memberRepository.save(new Member("userA"));
+        IntStream.range(0, 100)
+                .mapToObj(i -> new Member("user" + i, i))
+                .forEach(memberRepository::save);
     }
 }
--- src/main/java/study/datajpa/dto/MemberDto.java
+++ src/main/java/study/datajpa/dto/MemberDto.java
@@ -1,13 +1,24 @@
 package study.datajpa.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import study.datajpa.entity.Member;
 
 @Data
+@Builder
 @AllArgsConstructor
 public class MemberDto {
 
     private Long id;
     private String username;
     private String teamName;
+
+    public static MemberDto from(Member member) {
+        return MemberDto.builder()
+                .id(member.getId())
+                .username(member.getUsername())
+                .teamName(member.getTeam() != null ? member.getTeam().getName() : null)
+                .build();
+    }
 }
