eea80f8b8aa49992ab8ed264a35853cfbbd85d1d
hyunmin0317 <choihm9903@naver.com>
2024-03-24 14:21:58 +0900

    feat: Persistable 구현 (#6)

--- src/main/java/study/datajpa/entity/Item.java
+++ src/main/java/study/datajpa/entity/Item.java
@@ -1,16 +1,34 @@
 package study.datajpa.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.domain.Persistable;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-@Getter
+import java.time.LocalDateTime;
+
 @Entity
-public class Item {
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Item implements Persistable<String> {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private String id;
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    public Item(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean isNew() {
+        return createdDate == null;
+    }
 }
